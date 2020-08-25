7b13a89a36daf2fd8a01b9a15af655a3db3b77a5
Dhanashri <dhanashrinp3009@gmail.com>
2020-08-25 18:05:02 +0530

    Improve the test runner

--- src/indexTest.js
+++ src/indexTest.js
@@ -1,18 +1,24 @@
 const assert = require("assert");
-const  makeItQuestion = require("./index").makeItQuestion;
+const makeItQuestion = require("./index").makeItQuestion;
+let numberOfTestsPassed = 0;
+let numberOfTestsFailed = 0;
 
-runTest = (testName, functionToTest , argsForFunction , expected) => {
+runTest = (testName, functionToTest, argsForFunction, expected) => {
 
     console.log(`\n\t * Running Test - ${testName}`)
     const actual = functionToTest(...argsForFunction)
     try {
-        assert.strictEqual(actual,expected);
+        assert.strictEqual(actual, expected);
+        numberOfTestsPassed += numberOfTestsPassed + 1;
         console.log(" \n\t ++++++++++++++++++++ Test Passed +++++++++++++++++++ \n ")
     } catch (error) {
+        numberOfTestsFailed += numberOfTestsFailed + 1;
         console.log(`X X X X X X X \n Test Failed due to - \n ${error}`)
     }
-    
+
 };
 
-runTest("it should return yes-no type question for sentence" , makeItQuestion , ["you are idiot"] , "Are you idiot ?" )
-runTest("it should return yes-no type question for long sentence too" , makeItQuestion , ["he is going to market"] , "Is he going to market ?" )
+runTest("it should return yes-no type question for sentence", makeItQuestion, ["you are idiot"], "Are you idiot ?")
+runTest("it should return yes-no type question for long sentence too", makeItQuestion, ["he is going to market"], "Is he going to market ?")
+console.log(" ================================================ ");
+console.log(` Total test passed : ${numberOfTestsPassed} \n Total test failed : ${numberOfTestsFailed} \n`);
